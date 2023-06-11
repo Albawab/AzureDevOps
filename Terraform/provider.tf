5fa1af59d90ac3e07d65cba85341713099daf242
Albawab <albawab94@gmail.com>
2023-06-11 22:59:16 +0200

    Code terraform

---
+++ Terraform/provider.tf
@@ -0,0 +1,12 @@
+//"هده لكي يقمل مستقبل"
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
